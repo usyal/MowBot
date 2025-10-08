ede9d1542909c98f9b94b869f94fb37151d7f587
Uday Syal <udaysyal16@gmail.com>
2025-10-08 11:29:09 -0700

    Added Dataset folder and modules needed for classification

--- classification.py
+++ classification.py
@@ -1,1 +1,10 @@
 # AI model for classification
+import sklearn as sl
+# Stratified K fold makes sure the proportions are equal for testing/training data
+from sklearn.model_selection import StratifiedKFold as kfold
+# skimage allows for feature extraction
+from skimage import io, color, feature
+# RandomForestClassifier perform classification by construction decision trees
+# Commonly implemented with K Fold 
+from sklearn.ensemble import RandomForestClassifier as rfc
+import numpy as np
