a3c1ade674c24d20807caadefdbfc22e72f10538
Uday Syal <udaysyal16@gmail.com>
2025-10-08 18:59:46 -0700

    Modified Classifier

--- classification.py
+++ classification.py
@@ -8,3 +8,21 @@ from skimage import io, color, feature
 # Commonly implemented with K Fold 
 from sklearn.ensemble import RandomForestClassifier as rfc
 import numpy as np
+import os
+
+def feature_extractor(img_location):
+    pass
+
+
+features = []
+labels = []
+
+for label, folder in enumerate(["Grass", "Not-Grass"]):
+    path = os.path.join("Dataset", folder)
+    for img_file in os.listdir(path):
+        img_location = os.path.join(path, img_file)
+        features.append(feature_extractor(img_location))
+        labels.append(label)
+
+features = np.array(features)
+labels = np.array(labels)
