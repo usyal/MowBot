2c003660c183b03fa2726aaedfc8b7ba72ccf2d7
Uday Syal <udaysyal16@gmail.com>
2025-10-08 17:53:30 -0700

    Added image resizing to clean the original dataset

---
+++ image_resizer.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import os
+
+# Resizing the grass images
+for filename in os.listdir("Dataset/Grass"):
+    img_location = os.path.join("Dataset/Grass", filename)
+    img = Image.open(img_location)
+    new_img = img.resize((256, 256), Image.Resampling.LANCZOS)
+    new_img_location = os.path.join("Cleaned-Dataset/Grass", f"cleaned-{filename}")
+    new_img.save(new_img_location)
+
+# Resizing the non-grass images
+for filename in os.listdir("Dataset/Non-Grass"):
+    img_location = os.path.join("Dataset/Non-Grass", filename)
+    img = Image.open(img_location)
+    new_img = img.resize((256, 256), Image.Resampling.LANCZOS)
+    new_img_location = os.path.join("Cleaned-Dataset/Non-Grass", f"cleaned-{filename}")
+    new_img.save(new_img_location)
