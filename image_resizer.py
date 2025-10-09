c8236119af773ccc9a851525ff3567a4157c1a21
Uday Syal <udaysyal16@gmail.com>
2025-10-09 16:22:03 -0700

    Resized data

--- image_resizer.py
+++ image_resizer.py
@@ -13,6 +13,6 @@ for filename in os.listdir("Dataset/Grass"):
 for filename in os.listdir("Dataset/Non-Grass"):
     img_location = os.path.join("Dataset/Non-Grass", filename)
     img = Image.open(img_location)
-    new_img = img.resize((256, 256), Image.Resampling.LANCZOS)
+    new_img = img.resize((512, 512), Image.Resampling.LANCZOS)
     new_img_location = os.path.join("Cleaned-Dataset/Non-Grass", f"cleaned-{filename}")
     new_img.save(new_img_location)
