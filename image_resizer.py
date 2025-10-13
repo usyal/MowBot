1c9827ca53b02caba39ce1063d784dd208761702
Willy-Ho <williamho8813@gmail.com>
2025-10-13 12:28:16 -0700

    added 34 non grass, and 10 grass

--- image_resizer.py
+++ image_resizer.py
@@ -1,15 +1,16 @@
 from PIL import Image
 import os
 
-valid_extensions = ('.jpg') # William: Added this because it was adding some .DS_Store files from my anaconda, I will add a .gitignore in the future
+valid_extensions = ('.jpg', '.jpeg') # William: Added this because it was adding some .DS_Store files from my anaconda, I will add a .gitignore in the future
 
 # Resizing the grass images
 for filename in os.listdir("Dataset/Grass"):
-    img_location = os.path.join("Dataset/Grass", filename)
-    img = Image.open(img_location)
-    new_img = img.resize((224, 224), Image.Resampling.LANCZOS)
-    new_img_location = os.path.join("Cleaned-Dataset/Grass", f"cleaned-{filename}")
-    new_img.save(new_img_location)
+    if filename.lower().endswith(valid_extensions): #William: This currently only checks jpg files
+        img_location = os.path.join("Dataset/Grass", filename)
+        img = Image.open(img_location)
+        new_img = img.resize((224, 224), Image.Resampling.LANCZOS)
+        new_img_location = os.path.join("Cleaned-Dataset/Grass", f"cleaned-{filename}")
+        new_img.save(new_img_location)
 
 # Resizing the non-grass images
 for filename in os.listdir("Dataset/Non-Grass"):
